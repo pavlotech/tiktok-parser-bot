95bfaecf4ab596b196ab5ea83266300669003a7a
inoska <paveltarasenko799@gmail.com>
2023-11-03 00:03:19 +0100

    bug fixes

--- src/functions/isValidDate.ts
+++ src/functions/isValidDate.ts
@@ -1,14 +1,14 @@
 export default function isValidDate(dateString: string) {
-  var pattern = /^\d{2}-\d{2}-\d{4}$/;
+  var pattern = /^\d{2}.\d{2}.\d{4}$/;
   
   if (!pattern.test(dateString)) return false;
 
-  var parts = dateString.split("-");
-  var day = parseInt(parts[0], 10);
-  var month = parseInt(parts[1], 10);
+  var parts = dateString.split(".");
+  var month = parseInt(parts[0], 10);
+  var day = parseInt(parts[1], 10);
   var year = parseInt(parts[2], 10);
   
-  if (isNaN(day) || isNaN(month) || isNaN(year))return false;
+  if (isNaN(month) || isNaN(day) || isNaN(year)) return false;
   if (month < 1 || month > 12 || day < 1 || day > 31) return false;
   return true;
 }
--- src/telegram/index.ts
+++ src/telegram/index.ts
@@ -1,43 +1,25 @@
 import { Scenes, session } from 'telegraf';
 import SceneGenerator from '../types/SceneGenerator';
 import { bot } from '../../index';
-//import help from './commands/help';
+import help from './commands/help';
 import start from './commands/start';
 
 const lastUsageTimes: { [key: number]: number } = {};
 export async function loadTelegram() {
   try {
-    // commands
     bot.start(start);
 
-    const scene = new SceneGenerator();
-    const firstDate = scene.firstDate();
-    const secondDate = scene.secondDate();
-    const getName = scene.getName();
+    const scene = new SceneGenerator()
+    const firstDate = scene.firstDate(), secondDate = scene.secondDate(), getName = scene.getName();
 
-    const stage = new Scenes.Stage<Scenes.SceneContext>([firstDate, secondDate, getName], {
-      ttl: 10,
-    });
-
+    const stage = new Scenes.Stage<Scenes.SceneContext>([firstDate, secondDate, getName], { ttl: 10 * 60 * 1000 });
     bot.use(session());
     bot.use(stage.middleware());
-    
-    bot.command('get_stat', (ctx) => {
-      const userId = ctx.from.id;
-      const currentTime = Date.now();
 
-      if (lastUsageTimes[userId] && currentTime - lastUsageTimes[userId] < 0 * 60 * 1000) {
-        // Если команда использована ранее
-        ctx.reply("*Пожалуйста, подождите 10 минут перед следующим использованием команды /get_stat*", { parse_mode: 'Markdown' });
-      } else {
-        // Если команда используется впервые или прошло более 10 минут с предыдущего использования, обновите время последнего использования и выполните логику команды
-        lastUsageTimes[userId] = currentTime;
-        ctx.scene.enter("firstdate");
-      }
-    });
+    bot.command('get_stat', (ctx) => { ctx.scene.enter("firstdate") });
+    bot.command('help', help)
     bot.launch();
-    // launch
+
     return console.log('Connecting to Telegram')
   } catch (error) { console.log(error) }
-
 }
