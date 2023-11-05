e179a410f09516df3487e231ae196bc445919fb2
inoska <paveltarasenko799@gmail.com>
2023-11-05 11:26:31 +0100

    update handler

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
-import { Telegraf, Scenes } from 'telegraf';
-import { Launch } from './src/telegram';
-import dotenv from 'dotenv'; dotenv.config()
-
-const bot = new Telegraf<Scenes.SceneContext>(process.env.TG_TOKEN || '', { handlerTimeout: 60 * 60 * 1000 });
-console.log(new Launch().Telegram(bot))
+import { Launch } from './src/telegram/main';
+console.log(new Launch().Telegram())
